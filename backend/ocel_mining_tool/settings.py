79a48c7c9545f9dce948b5770ce1d2cda86efd07
AlirezaAmini8 <alirezaamini.138115@gmail.com>
2024-12-12 12:34:52 +0330

    Building Minio and Postgres using docker.

--- backend/ocel_mining_tool/settings.py
+++ backend/ocel_mining_tool/settings.py
@@ -83,10 +83,17 @@ DATABASES = {
         'NAME': 'ocel_db',
         'USER': 'postgres',
         'PASSWORD': 'password',
-        'HOST': 'localhost',
+        'HOST': 'postgres',
         'PORT': '5432',
     }
 }
+
+# Minio configuration
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = 'root'
+AWS_SECRET_ACCESS_KEY = 'password'
+AWS_STORAGE_BUCKET_NAME = 'ocel'
+AWS_S3_ENDPOINT_URL = 'http://minio:9000'
 
 
 # Password validation
